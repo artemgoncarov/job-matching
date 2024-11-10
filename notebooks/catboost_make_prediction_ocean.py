90bad9de4f9e0af82e4397f0f3c45a5bdf03ba57
Igor Kartashov <78425476+IGK-arch@users.noreply.github.com>
2024-11-10 10:37:20 +0300

    Update catboost_make_prediction_ocean.py

--- notebooks/catboost_make_prediction_ocean.py
+++ notebooks/catboost_make_prediction_ocean.py
@@ -3,6 +3,7 @@ import time
 import pandas as pd
 import librosa
 import numpy as np
+import tqdm
 from moviepy.editor import VideoFileClip
 from catboost import CatBoostRegressor
 from pandarallel import pandarallel
@@ -42,7 +43,6 @@ def process_video_for_prediction(VIDEO_FILE_PATH, AUDIO_DATA_DESTINATION):
         spectral_centroid = librosa.feature.spectral_centroid(y=y, sr=sr)
         spectral_centroid_mean = np.mean(spectral_centroid)
         return spectral_centroid_mean
-
     def extract_spectral_bandwidth_mean(audio_path):
         y, sr = librosa.load(audio_path, sr=None)
         spectral_bandwidth = librosa.feature.spectral_bandwidth(y=y, sr=sr)
@@ -54,6 +54,35 @@ def process_video_for_prediction(VIDEO_FILE_PATH, AUDIO_DATA_DESTINATION):
         zero_crossings = librosa.feature.zero_crossing_rate(y)
         zero_crossing_rate_mean = np.mean(zero_crossings)
         return zero_crossing_rate_mean
+
+    def process_emotion_predictions(data: pd.DataFrame) -> pd.DataFrame:
+        data[['happy', 'neutral', 'fear', 'disgust', 'angry']] = None 
+    
+        label_map = {
+            "LABEL_0": "happy",
+            "LABEL_1": "neutral",
+            "LABEL_2": "fear",
+            "LABEL_3": "disgust",
+            "LABEL_4": "angry",
+            "LABEL_5": "surprise", 
+        }
+        for idx, row in tqdm(data.iterrows(), total=data.shape[0], desc="Processing Audio Files"):
+    
+            predictions = pipe(row['audio_path'])
+            happiness = {label_map[f"LABEL_{i}"]: 0 for i in range(len(label_map))}
+    
+            # Map predictions to the happiness dictionary
+            for pred in predictions:
+                happiness[label_map[pred['label']]] = pred["score"]
+    
+            # Now, assign the predicted values to the respective columns
+            data.at[idx, 'happy'] = happiness['happy']
+            data.at[idx, 'neutral'] = happiness['neutral']
+            data.at[idx, 'fear'] = happiness['fear']
+            data.at[idx, 'disgust'] = happiness['disgust']
+            data.at[idx, 'angry'] = happiness['angry']
+    
+        return data
         
     wav_file_path = convert_video_to_wav(VIDEO_FILE_PATH, AUDIO_DATA_DESTINATION)
 
@@ -62,24 +91,24 @@ def process_video_for_prediction(VIDEO_FILE_PATH, AUDIO_DATA_DESTINATION):
         return None
     df = pd.DataFrame(data={"audio_path": [wav_file_path]})
 
-    df["spectral_centroid_mean"] = df["audio_path"].apply(extract_spectral_centroid_mean)
-    df["spectral_bandwidth"] = df["audio_path"].apply(extract_spectral_bandwidth_mean)
-    df["zero_crossing_rate"] = df["audio_path"].apply(extract_zero_crossing_rate_mean)
-    data = df[['spectral_centroid_mean', 'spectral_bandwidth', 'zero_crossing_rate']]
-    
-    return data
+    df["spectral_centroid_mean"] = df["audio_path"].parallel_apply(extract_spectral_centroid_mean)
+    df["spectral_bandwidth"] = df["audio_path"].parallel_apply(extract_spectral_bandwidth_mean)
+    df["zero_crossing_rate"] = df["audio_path"].parallel_apply(extract_zero_crossing_rate_mean)
+    df = process_emotion_predictions(df)
+
+    data = df[["spectral_bandwidth", "zero_crossing_rate", "spectral_centroid_mean", "happy", "neutral", "fear", "disgust", "angry"]]
+
     model_conscientiousness = CatBoostRegressor()
     model_extraversion = CatBoostRegressor()
     model_neuroticism = CatBoostRegressor()
     model_openness = CatBoostRegressor()
     model_agreeableness = CatBoostRegressor()
 
-    model_conscientiousness.load_model(r"D:\Projects\CP_megnar\job-matching\models_artem\conscientiousness_best_model.cbm")
-    model_extraversion.load_model(r"D:\Projects\CP_megnar\job-matching\models_artem\extraversion_best_model.cbm")
-    model_neuroticism.load_model(r"D:\Projects\CP_megnar\job-matching\models_artem\neuroticism_best_model.cbm")
-    model_openness.load_model(r"D:\Projects\CP_megnar\job-matching\models_artem\openness_best_model.cbm")
-    model_agreeableness.load_model(r"D:\Projects\CP_megnar\job-matching\models_artem\agreeableness_best_model.cbm")
-
+    model_conscientiousness.load_model("conscientiousness_best_model_audio_features.cbm")
+    model_extraversion.load_model("extraversion_best_model_audio_features.cbm")
+    model_neuroticism.load_model("neuroticism_best_model_audio_features.cbm")
+    model_openness.load_model("openness_best_model_audio_features.cbm")
+    model_agreeableness.load_model("agreeableness_best_model_audio_features.cbm")
     predict_model_agreeableness = model_agreeableness.predict(data)
     predict_model_extraversion = model_extraversion.predict(data)
     predict_model_openness = model_openness.predict(data)
@@ -95,8 +124,7 @@ def process_video_for_prediction(VIDEO_FILE_PATH, AUDIO_DATA_DESTINATION):
     }
 
     return answers
-
-
+    
 results_df = pd.DataFrame(columns=["filename", "conscientiousness", "extraversion", "neuroticism", "openness", "agreeableness"])
 
 VIDEO_FOLDER_PATH = r'D:\Projects\CP_megnar\test_dataset_vprod_encr_test\test_data\test000'
